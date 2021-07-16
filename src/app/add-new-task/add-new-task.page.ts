60c88ea7e2a8bf08fa4498e598450c926bc927d0
charlenebatiste <batistecharlene@gmail.com>
2021-07-16 12:34:06 -0400

    create input fields for task form, models and a loop to display categories

--- src/app/add-new-task/add-new-task.page.ts
+++ src/app/add-new-task/add-new-task.page.ts
@@ -7,6 +7,14 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./add-new-task.page.scss'],
 })
 export class AddNewTaskPage implements OnInit {
+  // add available categories for user to choose from when creating a new task
+  categories = ['work', 'personal', 'home'];
+
+  taskName;
+  taskDate;
+  taskPriority;
+  taskCategory;
+
   constructor(public modalCtrl: ModalController) {}
 
   ngOnInit() {}
